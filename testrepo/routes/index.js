f99e54d310462ee67aa3976afc8a211006decfec
DanielCech <daniel.cech@gmail.com>
2015-05-26 14:25:36 +0200

    Initial commit

---
+++ testrepo/routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET home page. */
+router.get('/hello', function(req, res, next) {
+  var data = {
+    message: 'Hello world!'
+  };
+  res.send(data);
+});
+
+module.exports = router;
